a573368a105a20dc521e0a9992757598cf3f6345
Donuttouchme <59120493+Donuttouchme@users.noreply.github.com>
2026-05-18 12:54:25 +0200

    chore(frontend): delete unused V11MigrationToast (served its purpose in v1.1)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { useHeartbeat } from "./hooks/useHeartbeat";
-import { V11MigrationToast } from "./components/V11MigrationToast";
 
 export default function App() {
   useHeartbeat();
-  return (
-    <>
-      <V11MigrationToast />
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
